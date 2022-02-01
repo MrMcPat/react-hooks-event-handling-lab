e31103c079bc9b43be7c1411d4927a476c0ccc22
Patrick Liang <pl204511194@yahoo.com>
2022-02-01 09:52:09 -0500

    commit tree ecc7962e7e437828b23ae79aff6f26af2cf29d0d

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+const EyesOnMe = () => {
+  function blurHandler(event) {
+    console.log("Hey! Eyes on me!")
+  }
+  
+  function focusHandler(event) {
+    console.log("Good!")
+  }
+    
+  return <div>
+      <button onBlur={blurHandler} onFocus={focusHandler}>Eyes on me</button>
+  </div>;
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+import React from 'react';
+
+const Keypad = () => {
+    function passwordHandler(event) {
+        console.log("Entering password...")
+    }
+
+  return <div>
+      <input type="password" name="search" placeholder="Enter your password..." onChange={passwordHandler}/>
+  </div>;
+};
+
+export default Keypad;
